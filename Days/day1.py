f64ff2e0a24b72b607540216f2902f37c7652898
Justin Powell <somerandomchemist@gmail.com>
2023-02-02 17:16:13 -0600

    Finished Day1 project

---
+++ Days/day1.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+
+# This course follows the 100-days-of-code-challenge from the udemy course : 100-days-of-code
+# While some content will be remedial, basic, etc. I feel it best serves me to do all task and complete my 100 days
+# Justin Powell
+
+a = input("What is the name of your current city?\n")
+b = input("What is your pet's name?\n")
+print("\nYour band name is:")
+print(f"{a} {b}\n")
