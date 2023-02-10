1279ef20aa45f1de39ae6e7b9a5ff63dad7d473f
Justin Powell <somerandomchemist@gmail.com>
2023-02-10 16:43:53 -0600

    day 9 starting

---
+++ Days/day9-BlindAuction.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+# This course follows the 100-days-of-code-challenge from the udemy course : 100-days-of-code
+# While some content will be remedial, basic, etc. I feel it best serves me to do all task and complete my 100 days
+# Justin Powell
+
+# Dictionaries, list, nesting one in another or two of the same or whatever
+
+# Dictionaries in python have a key and a value that is associated to that key
+
+# {key: value, key2: value2} is a dictionary.
+
+# to add to dictionary:
+#dictionary_1["key3"] = "Value3"
+
+#Initialize or clear
+#dictionary_2 = {}
+
+#edit
+#dictionary_1["key3"] = "value4"
+
+#Looping it
+#for key in dictionary_1:
+#   print(key) # its the key
+#   print(dictionary_1[key]) # its the value
